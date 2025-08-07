be085a3ea2f15d8dd50ddbdac6d24bd41bb95aff
George Freelance Developer <112611533+GeorgeFreelanceDeveloper@users.noreply.github.com>
2025-08-08 01:05:04 +0200

    Added S&P Medium cap momentum and S&P Small ca momentum indexes

--- strategy/stock_index_top_10.py
+++ strategy/stock_index_top_10.py
@@ -7,9 +7,11 @@ import datetime
 class StockIndexTop10_V2(QCAlgorithm):
 
     INDEXES = {
-            "S&P500": ["NVDA","MSFT","AAPL","AMZN","META","AVGO","GOOGL","BRK.B","TSLA", "GOOG"], # https://finance.yahoo.com/quote/SPY/holdings/
+            "SP500": ["NVDA","MSFT","AAPL","AMZN","META","AVGO","GOOGL","BRK.B","TSLA", "GOOG"], # https://finance.yahoo.com/quote/SPY/holdings/
             "NASDAQ100":["NVDA","MSFT","AAPL","AMZN","AVGO","META","NFLX","TSLA","COST","GOOGL"], # https://finance.yahoo.com/quote/QQQ/holdings/
-            "S&P500 MOMENTUM": ["NVDA","META","AMZN","AVGO","JPM","TSLA","WMT","NFLX","PLTR","COST"] # https://finance.yahoo.com/quote/SPMO/holdings/ 
+            "SP500 MOMENTUM": ["NVDA","META","AMZN","AVGO","JPM","TSLA","WMT","NFLX","PLTR","COST"], # https://finance.yahoo.com/quote/SPMO/holdings/ 
+            "SP MEDIUM CAP MOMENTUM": ["IBKR","EME","SFM","FIX","GWRE","USFD","CRS","EQH","CW","CASY"], # https://finance.yahoo.com/quote/XMMO/holdings/ 
+            "SP SMALL CAP MOMENTUM": ["EAT","CORT","COOP","AWI","IDCC","SKYW","JXN","CALM","DY","SMTC"] # https://finance.yahoo.com/quote/XSMO/holdings/ 
         }
 
     def initialize(self):
@@ -17,7 +19,7 @@ class StockIndexTop10_V2(QCAlgorithm):
         # ********************************
         # User defined inputs
         # ********************************
-        index = self.get_parameter("index", "S&P500 MOMENTUM")
+        index = self.get_parameter("index", "SP500 MOMENTUM")
         rebalancing_frequency = self.get_parameter("rebalancing_frequency","weekly")
         self.enable_filter = True if (self.get_parameter("enable_filter", "True") == "True") else False
 
@@ -32,7 +34,7 @@ class StockIndexTop10_V2(QCAlgorithm):
         # ********************************
 
         # Basic
-        self.set_start_date(datetime.date.today().year - 8, 1, 1)
+        self.set_start_date(datetime.date.today().year - 10, 1, 1)
         self.set_cash(10000)
         self.markets = {symbol: self.add_equity(symbol, Resolution.DAILY) for symbol in self.symbols}
         self.enable_trading = True
@@ -74,4 +76,4 @@ class StockIndexTop10_V2(QCAlgorithm):
         if self.enable_trading:
             for symbol in self.symbols:
                 self.set_holdings(symbol, 1/len(self.symbols))
-                # self.set_holdings(symbol, 1/len(self.symbols)*2) leverage 2x
+                # self.set_holdings(symbol, 1/len(self.symbols)*2) # leverage 2x
