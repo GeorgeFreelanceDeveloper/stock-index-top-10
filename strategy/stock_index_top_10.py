9f738bb4be405515fea5159b6082098cd81a2521
GeorgeFreelanceDeveloper <112611533+GeorgeFreelanceDeveloper@users.noreply.github.com>
2025-08-08 19:29:01 +0200

    added support leverage trading + added backtests

--- strategy/stock_index_top_10.py
+++ strategy/stock_index_top_10.py
@@ -5,14 +5,13 @@ import datetime
 
 ## Trend follow strategy selecting the top 10 companies from the stock index with weekly/monthly rebalancing equally weighted.
 class StockIndexTop10_V2(QCAlgorithm):
-
     INDEXES = {
-            "SP500": ["NVDA","MSFT","AAPL","AMZN","META","AVGO","GOOGL","BRK.B","TSLA", "GOOG"], # https://finance.yahoo.com/quote/SPY/holdings/
-            "NASDAQ100":["NVDA","MSFT","AAPL","AMZN","AVGO","META","NFLX","TSLA","COST","GOOGL"], # https://finance.yahoo.com/quote/QQQ/holdings/
-            "SP500 MOMENTUM": ["NVDA","META","AMZN","AVGO","JPM","TSLA","WMT","NFLX","PLTR","COST"], # https://finance.yahoo.com/quote/SPMO/holdings/ 
-            "SP MEDIUM CAP MOMENTUM": ["IBKR","EME","SFM","FIX","GWRE","USFD","CRS","EQH","CW","CASY"], # https://finance.yahoo.com/quote/XMMO/holdings/ 
-            "SP SMALL CAP MOMENTUM": ["EAT","CORT","COOP","AWI","IDCC","SKYW","JXN","CALM","DY","SMTC"] # https://finance.yahoo.com/quote/XSMO/holdings/ 
-        }
+        "SP500": ["NVDA", "MSFT", "AAPL", "AMZN", "META", "AVGO", "GOOGL", "BRK.B", "TSLA", "GOOG"], # https://finance.yahoo.com/quote/SPY/holdings/
+        "NASDAQ100": ["NVDA", "MSFT", "AAPL", "AMZN", "AVGO", "META", "NFLX", "TSLA", "COST", "GOOGL"], # https://finance.yahoo.com/quote/QQQ/holdings/
+        "SP500 MOMENTUM": ["NVDA", "META", "AMZN", "AVGO", "JPM", "TSLA", "WMT", "NFLX", "PLTR", "COST"], # https://finance.yahoo.com/quote/SPMO/holdings/
+        "SP MEDIUM CAP MOMENTUM": ["IBKR", "EME", "SFM", "FIX", "GWRE", "USFD", "CRS", "EQH", "CW", "CASY"], # https://finance.yahoo.com/quote/XMMO/holdings/
+        "SP SMALL CAP MOMENTUM": ["EAT", "CORT", "COOP", "AWI", "IDCC", "SKYW", "JXN", "CALM", "DY", "SMTC"] # https://finance.yahoo.com/quote/XSMO/holdings/
+    }
 
     def initialize(self):
 
@@ -20,7 +19,7 @@ class StockIndexTop10_V2(QCAlgorithm):
         # User defined inputs
         # ********************************
         index = self.get_parameter("index", "SP500 MOMENTUM")
-        rebalancing_frequency = self.get_parameter("rebalancing_frequency","weekly")
+        rebalancing_frequency = self.get_parameter("rebalancing_frequency", "monthly")
         self.enable_filter = True if (self.get_parameter("enable_filter", "True") == "True") else False
 
         # Basic settings
@@ -28,13 +27,14 @@ class StockIndexTop10_V2(QCAlgorithm):
         self.benchmark_symbol = self.get_parameter("benchmark_symbol", "SPMO")
         self.add_equity(self.benchmark_symbol, Resolution.DAILY)
         self.benchmark_sma200 = self.sma(self.benchmark_symbol, 200)
+        self.leverage = self.get_parameter("leverage", 0)
 
         # ********************************
         # Algorithm settings
         # ********************************
 
         # Basic
-        self.set_start_date(datetime.date.today().year - 10, 1, 1)
+        self.set_start_date(datetime.date.today().year - 5, 1, 1)
         self.set_cash(10000)
         self.markets = {symbol: self.add_equity(symbol, Resolution.DAILY) for symbol in self.symbols}
         self.enable_trading = True
@@ -49,7 +49,7 @@ class StockIndexTop10_V2(QCAlgorithm):
                 rebalancing_frequency_internal = self.DateRules.YearStart(self.symbols[0])
             case _:
                 rebalancing_frequency_internal = self.DateRules.MonthStart(self.symbols[0])
-        
+
         self.Schedule.On(
             rebalancing_frequency_internal,
             self.TimeRules.AfterMarketOpen(self.symbols[0], 30),
@@ -75,5 +75,4 @@ class StockIndexTop10_V2(QCAlgorithm):
 
         if self.enable_trading:
             for symbol in self.symbols:
-                self.set_holdings(symbol, 1/len(self.symbols))
-                # self.set_holdings(symbol, 1/len(self.symbols)*2) # leverage 2x
+                self.set_holdings(symbol, (1 / len(self.symbols)) + (1 / len(self.symbols) * self.leverage))
